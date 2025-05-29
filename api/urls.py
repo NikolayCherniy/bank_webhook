4aac0e8e32b834a0473205511de751ad0991e64f
nikolay.chernyi <nikolay.chernyi@redcollar.ru>
2025-05-29 17:23:03 +0500

    Make inn param string in URL patterns.

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from api.views import BankWebhookView, OrganizationBalanceView
 
 urlpatterns = [
     path("webhook/bank/", BankWebhookView.as_view()),
-    path("organizations/<int:inn>/balance/", OrganizationBalanceView.as_view()),
+    path("organizations/<str:inn>/balance/", OrganizationBalanceView.as_view()),
 ]
